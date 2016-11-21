e04d631afbf4009d1e45149ca63639af858677c9
韩帅宾(幻影) <hansb@youzu.com>
2016-11-21 15:23:36 +0800

    add lib log

--- LoveClient/lib/init.lua
+++ LoveClient/lib/init.lua
@@ -3,12 +3,13 @@
 -- 全局的引用
 require("util")
 class = require("middleclass")
-
+log = require("log.log")
 
 local lib = {}
 
 lib.ui = require("LoveFrames.init")
 lib.net = require("LoverNet.lovernet")
 lib.flux = require("flux.flux")
+
 
 return lib
